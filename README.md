8c812b3dfefd79ed46ee858c9f28af7ab8ba9a79
Jeremy Bahadirli <jeremybahadirli@gmail.com>
2024-01-26 17:50:48 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# YouTube Scrollbar Remover
+**YouTube Scrollbar Remover** is a Userscripts ([GitHub](https://github.com/quoid/userscripts), [Mac App Store](https://apps.apple.com/us/app/userscripts/id1463298887)) file which removes YouTube's custom scrollbar and enables the macOS system scrollbar.
---
+++ yt-scrollbar-remover.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name        YouTube Scrollbar Remover
+// @match       *://*.youtube.com/*
+// @version 1.0
+// @updateURL 
+// ==/UserScript==
+
+outside:
+for (const sheet of document.styleSheets) {
+    try {
+        for (let i = 0; i < sheet.cssRules.length; i++) {
+            if (/body::-webkit-scrollbar/.exec(sheet.cssRules[i].selectorText)) {
+                sheet.deleteRule(i);
+                document.body.style.display = 'none';
+                setTimeout(() => document.body.style.display = '', 0);
+                break outside;
+            }
+        }
+    } catch (e) {
+    }
+}

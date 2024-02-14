f2f4d90565c2b97a11359f2b51929b8294a3d187
Jeremy Bahadirli <jeremybahadirli@gmail.com>
2024-02-14 17:57:51 -0600

    Refactor logic from functions to the global scope

--- yt-scrollbar-remover.js
+++ yt-scrollbar-remover.js
@@ -5,28 +5,22 @@
 // @updateURL   https://raw.githubusercontent.com/jeremybahadirli/youtube-scrollbar-remover/main/yt-scrollbar-remover.js
 // ==/UserScript==
 
-function removeWebkitScrollbarRule() {
-    for (const sheet of document.styleSheets) {
-        try {
-            for (let i = 0; i < sheet.cssRules.length; i++) {
-                if (sheet.cssRules[i].selectorText === 'body::-webkit-scrollbar') {
-                    sheet.deleteRule(i);
-                    refreshBody();
-                    return;
-                }
+for (const sheet of document.styleSheets) {
+    try {
+        for (let i = 0; i < sheet.cssRules.length; i++) {
+            if (sheet.cssRules[i].selectorText === 'body::-webkit-scrollbar') {
+                sheet.deleteRule(i);
+                // Refresh body styling
+                const overflowY = document.body.style.overflowY;
+                document.body.style.overflowY = 'hidden';
+                setTimeout(() => document.body.style.overflowY = overflowY, 0);
+                
+                return;
             }
-        } catch (e) {
-            if (!(e instanceof DOMException && e.name === 'SecurityError')) {
-                console.error("YouTube Scrollbar Remover", e.name, e.message);
-            }
+        }
+    } catch (e) {
+        if (!(e instanceof DOMException && e.name === 'SecurityError')) {
+            console.error("YouTube Scrollbar Remover", e.name, e.message);
         }
     }
 }
-
-function refreshBody() {
-    const overflowY = document.body.style.overflowY;
-    document.body.style.overflowY = 'hidden';
-    setTimeout(() => document.body.style.overflowY = overflowY, 0);
-}
-
-removeWebkitScrollbarRule();
